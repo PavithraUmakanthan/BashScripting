7842bdb7e3eeb58bd112347db6e2e9c84430dc85
Pavi Uma <pavi@1strategy.com>
2018-05-30 16:52:02 -0700

    added comments to the fizzbuzz  code

---
+++ fizzbuzz.sh
@@ -0,0 +1,10 @@
+#first function that prints the return value from the second function
+
+#second function that returns each number from 1 to 100 by replacing the number with fizz or buzz or fizzbuzz
+#input (parameters) = 1 number from 1 to 100
+#work:
+#	Step1: check whether the number is between 1 to 100.
+#	Step2: check whether the number is divisible by both 3 &5.
+#	Step3: check whether the number is divisible only by 3.
+#	Step4: check whether the number is divisible only by 5.
+#output (print) = fizz or buzz or fizzbuzz
