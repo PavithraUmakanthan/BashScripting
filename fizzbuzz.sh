bdd92503ce99c7dbc70ea21e6f275a00d41268c2
Pavi Uma <pavi@1strategy.com>
2018-05-31 12:07:17 -0700

    Added test cases for the second function

--- fizzbuzz.sh
+++ fizzbuzz.sh
@@ -1,13 +1,30 @@
 #first function that prints the return value from the second function
 #work: 
-#   Step1:Call the second fuction passing one argument.
-#   Step2:Get the return value from second function and print the value.
+#   Step1:the loop will run 100 times, once for each number between 1 and 100
+#   Step2:it will pass that number as argument to the second function
+#output (print): print the return value from the second function.
 
 #second function that returns each number from 1 to 100 by replacing the number with fizz or buzz or fizzbuzz
 #input (parameters) = 1 number from 1 to 100
 #work:
 #	Step1: check whether the number is between 1 to 100.
-#	Step2: check whether the number is divisible by both 3 &5.
+#	Step2: check whether the number is divisible by both 3&5.
 #	Step3: check whether the number is divisible only by 3.
 #	Step4: check whether the number is divisible only by 5.
 #output (return) = fizz or buzz or fizzbuzz or number itself
+
+#Test Cases for the second function:
+#Case1:
+#      input:4
+#      output:4
+#Case2:
+#      input:15
+#      output:fizzbuzz
+#Case3:
+#      input:20
+#      output:buzz
+#Case4:
+#      input:21
+#      output:fizz
+
+
