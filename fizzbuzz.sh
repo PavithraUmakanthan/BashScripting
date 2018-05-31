aaca31a84fa64897268210e408299ddc61f77e2a
Pavi Uma <pavi@1strategy.com>
2018-05-30 22:15:16 -0700

    Added changes to the comments

--- fizzbuzz.sh
+++ fizzbuzz.sh
@@ -1,4 +1,7 @@
 #first function that prints the return value from the second function
+#work: 
+#   Step1:Call the second fuction passing one argument.
+#   Step2:Get the return value from second function and print the value.
 
 #second function that returns each number from 1 to 100 by replacing the number with fizz or buzz or fizzbuzz
 #input (parameters) = 1 number from 1 to 100
@@ -7,4 +10,4 @@
 #	Step2: check whether the number is divisible by both 3 &5.
 #	Step3: check whether the number is divisible only by 3.
 #	Step4: check whether the number is divisible only by 5.
-#output (print) = fizz or buzz or fizzbuzz
+#output (return) = fizz or buzz or fizzbuzz or number itself
